54b047615f1bec5da35b7e15c9e26e5973f40f15
Juan Núñez <admin@juannunezblasco.es>
2024-04-07 18:30:53 +0200

    fix: wrong h1

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 
-const Blog = async () => {
+const About = async () => {
 
   await new Promise<void>((resolve) => setTimeout(() => resolve(), 300));
 
   return (
     <div className="bg-rose-400">
       <div className="container flex flex-col items-center content-center justify-between h-[90vh] mx-auto">
-        <h1 className="text-white my-auto font-extrabold text-xl">Blog</h1>
+        <h1 className="text-white my-auto font-extrabold text-xl">About</h1>
       </div>
     </div>
   )
 }
 
-export default Blog
+export default About
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 
-const About = async () => {
+const Blog = async () => {
 
   await new Promise<void>((resolve) => setTimeout(() => resolve(), 300));
 
   return (
     <div className="bg-emerald-700">
       <div className="container flex flex-col items-center content-center justify-between h-[90vh] mx-auto">
-        <h1 className="text-white my-auto font-extrabold text-xl">About</h1>
+        <h1 className="text-white my-auto font-extrabold text-xl">Blog</h1>
       </div>
     </div>
   )
 }
 
-export default About
+export default Blog
